b335d2ca0ea3a907985e3e20aab09b41f3b222c8
jayptl11 <phamlong2k3@gmail.com>
2024-11-04 23:01:45 +0700

    fix reply

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -28,7 +28,7 @@ public class DBContext {
     }
 
     public DBContext() {
-        this("jdbc:sqlserver://localhost:1433;databaseName=mk9;encrypt=true;trustServerCertificate=true",
+        this("jdbc:sqlserver://localhost:1433;databaseName=mk10;encrypt=true;trustServerCertificate=true",
                 "sa","123456");
 
     }
